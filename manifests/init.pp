31fe7a3088d4ea5b4473400372c147cb49a5a261
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2015-04-10 11:48:46 +0200

    #10459: Rewrite of github module

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,147 +5,69 @@
 #
 # === Parameters:
 #
-# The class supports nearly all of the parameters supported by rhnreg_ks
-#
-#   [*profilename*]
-#     The name the system should use in RHN or Satellite
-#   [*username*]
-#     The username to use when registering the system
-#   [*password*]
-#     The password to use when registering the system
 #   [*activationkey*]
 #     The activation key to use when registering the system (cannot be used with
 #     username and password) - This is the recommended way so that credentials
 #     aren't stored in reports.
-#   [*hardware*]
-#     Whether or not the hardware information should be probed (default: true)
-#   [*packages*]
-#     Whether or not packages information should be probed (default: true)
-#   [*virtinfo*]
-#     Whether or not virtualiztion information should be uploaded (default: true)
-#   [*rhnsd*]
-#     Whether or not rhnsd should be started after registering (default: true)
+#
+#   [*sslca_source*]
+#     File source of the SSL CA to use.
+#
 #   [*force*]
 #     Should the registration be forced.  Use this option with caution, setting it
 #     to true will cause the rhnreg_ks command to be run every time puppet runs
 #     (default: false)
-#   [*proxy*]
-#     If needed, specify the HTTP Proxy
-#   [*proxyuser*]
-#     Username to use for the proxy server
-#   [*proxypass*]
-#     Password to use for the proxy server
-#   [*sslca*]
-#     Path to the SSL CA to use.  If needed, it usually looks like
-#     /usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT
-#   [*serverurl*]
-#     URL to the server to register with
 #
-# === Examples
 #
-# class { 'rhn_register':
-#    sslca         => '/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT',
-#    serverurl     => 'https://my-satellite-server.example.com/XMLRPC',
-#    activationkey => '1-activationkeyiwanttouse',
-# }
-#
-# class { 'rhn_register':
-#    activationkey => '1-activationkeyiwanttouseforrhn',
-# }
 #
 # === Authors
 #
-# Phil Fenstermacher <pcfens@wm.edu>
-#
-class rhn_register (
-  $profilename   = undef,
-  $username      = undef,
-  $password      = undef,
-  $activationkey = undef,
-  $hardware      = true,
-  $packages      = true,
-  $virtinfo      = true,
-  $rhnsd         = true,
-  $force         = false,
-  $proxy         = undef,
-  $proxyuser     = undef,
-  $proxypass     = undef,
-  $sslca         = undef,
-  $serverurl     = undef,
-){
-  if $::osfamily != 'RedHat' and $::operatingsystem != 'Solaris' {
-    fail("You can't register ${::operatingsystem} with RHN or Satellite using this puppet module")
+# Raymond Kristiansen <raymond@uib.no>
+# 
+class rhn_register(
+  $activationkey,
+  $org = undef,
+  $sslca_source = undef,
+  $serverurl = undef,
+  $sslca_path = '/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT',
+  $command = '/usr/sbin/subscription-manager',
+  $force = false,
+) {
+
+  if $sslca_source {
+    file { $sslca_path:
+      source => $sslca,
+      owner => 'root',
+      group => 'root',
+      mode => '0600'
+    }
   }
 
-  if $rhn_register::username == undef and $rhn_register::activationkey == undef {
-    fail('Either an activation key or username/password is required to register')
-  }
 
-  $profile_name = $rhn_register::profilename ? {
+  $activation_key = $activationkey ? {
     undef   => '',
-    default => " --profilename ${rhn_register::profilename}",
+    default => " --activationkey ${activationkey}",
   }
 
-  $rhn_login = $rhn_register::username ? {
-    undef   => '',
-    default => " --username ${rhn_register::username} --password ${rhn_register::password}",
+  $use_org = $org ? {
+    false   => '',
+    default => "--org ${org}",
   }
 
-  $activation_key = $rhn_register::activationkey ? {
-    undef   => '',
-    default => " --activationkey ${rhn_register::activationkey}",
-  }
+  $command_args = "${activation_key}${use_org}"
 
-  $send_hardware = $rhn_register::hardware ? {
-    false   => ' --nohardware',
-    default => '',
-  }
-
-  $send_packages = $rhn_register::packages ? {
-    false   => ' --nopackages',
-    default => '',
-  }
-
-  $send_virtinfo = $rhn_register::virtinfo ? {
-    false   => ' --novirtinfo',
-    default => '',
-  }
-
-  $start_rhnsd = $rhn_register::rhnsd ? {
-    false   => ' --norhnsd',
-    default => '',
-  }
-
-  $proxy_login = $rhn_register::proxyuser ? {
-    undef   => '',
-    default => " --proxyUser ${rhn_register::proxyuser} --proxyPassword ${rhn_register::proxypass}",
-  }
-
-  $proxy_server = $rhn_register::proxy ? {
-    undef   => '',
-    default => " --proxy ${rhn_register::proxy}",
-  }
-
-  $ssl_ca = $rhn_register::sslca ? {
-    undef   => '',
-    default => " --sslCACert ${rhn_register::sslca}",
-  }
-
-  $server_url = $rhn_register::serverurl ? {
-    undef   => '',
-    default => " --serverUrl ${rhn_register::serverurl}",
-  }
-
-  $command_args = "${rhn_register::profile_name}${rhn_register::activation_key}${rhn_register::rhn_login}${rhn_register::send_hardware}${rhn_register::send_packages}${rhn_register::send_virtinfo}${rhn_register::start_rhnsd}${rhn_register::proxy_server}${rhn_register::proxy_login}${rhn_register::ssl_ca}${rhn_register::server_url}"
-
-  if $rhn_register::force {
+  if $force and $::operatingsystem == 'RedHat' {
     exec { 'register_with_rhn':
-      command => "/usr/sbin/rhnreg_ks --force${rhn_register::command_args}",
+      command => "${command} register --force --auto-attach${command_args}",
+      notify => Exec['rhn_auto_attach']
+    }
+  } elsif $::operatingsystem == 'RedHat' {
+    exec { 'register_with_rhn':
+      command => "${command} register --auto-attach${command_args}",
+      unless => "${command} list | /bin/grep Subscribed",
     }
   } else {
-    exec { 'register_with_rhn':
-      command => "/usr/sbin/rhnreg_ks${rhn_register::command_args}",
-      creates => '/etc/sysconfig/rhn/systemid',
-    }
+    notify { "${command} register --auto-attach${command_args}": }
   }
+
 }
